d6dedc491e0d2402a3d1b059d5dbceda756e557c
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-03 09:21:03 +0600

    learning swap js and random js

---
+++ random.js
@@ -0,0 +1,16 @@
+var num = 2.35453;
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+var dice = Math.random(num);
+
+var randomNum = Math.random() * 10;
+var output = Math.round(randomNum);
+
+// console.log(output);
+
+for( var i = 0; i <10; i++){
+	var randomNum = Math.random() * 6;
+	var output = Math.round(randomNum);
+	console.log(output);
+}
